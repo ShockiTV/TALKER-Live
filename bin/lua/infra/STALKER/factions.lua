75842888a9c651f818a8f24b344753561158bace
Coelacanthiform <coelacanthiform@proton.me>
2025-11-23 12:56:10 +0100

    Minor update to factions.lua to fit new prompt structure

--- bin/lua/infra/STALKER/factions.lua
+++ bin/lua/infra/STALKER/factions.lua
@@ -3,55 +3,55 @@ local logger = require('framework.logger')
 local factions = {
     killer = {
         name = "Mercenary",
-        style = "a member of the Mercenary faction, a Private Military Company from the US operating in the Zone where they offer their services to whoever is willing to pay. They are tactical, mercenary, morally flexible, confident, casual and respect nobody except other mercenaries and the UNISG. "
+        style = "member of the Mercenary faction, a Private Military Company from the US operating in the Zone where they offer their services to whoever is willing to pay. They are tactical, mercenary, morally flexible, confident, casual and respect nobody except other mercenaries and the UNISG. "
     },
     dolg = {
         name = "Duty",
-        style = "a member of Duty, a faction that despises the Zone and wants to protect people from it. They are authoritarian, brusque and emotionally strained. "
+        style = "member of Duty, a faction that despises the Zone and wants to protect people from it. They are authoritarian, brusque and emotionally strained. "
     },
     freedom = {
         name = "Freedom",
-        style = "a member of Freedom, a faction that dislikes authority and wants the Zone to be open to all. They are rebellious, relaxed and expressive. "
+        style = "member of Freedom, a faction that dislikes authority and wants the Zone to be open to all. They are rebellious, relaxed and expressive. "
     },
     bandit = {
         name = "Bandit",
-        style = "a member of the Bandit faction, a group of gopniks and a vatniks. They are vulgar and rude but like to crack jokes frequently, are opportunistic and lawless but often cowardly. "
+        style = "member of the Bandit faction, a group of gopniks and a vatniks. They are vulgar and rude but like to crack jokes frequently, are opportunistic and lawless but often cowardly. "
     },
     monolith = {
         name = "Monolith",
-        style = "a member of the Monolith faction. They are a brainwashed cult worshipping the Monolith and are fanatical, cryptic, zealous and emotionally void. They retain very little of their memories and personality from before their brainwashing. "
+        style = "member of the Monolith faction. They are a brainwashed cult worshipping the Monolith and are fanatical, cryptic, zealous and emotionally void. They retain very little of their memories and personality from before their brainwashing. "
     },
     stalker = {
         name = "stalker",
-        style = "a member of the Loner faction of scavengers, artifact hunters and explorers who are in the zone illegally. They are plain-spoken, adaptive, optimistic, quietly authentic and emotionally scarred. "
+        style = "member of the Loner faction of scavengers, artifact hunters and explorers who are in the zone illegally. They are plain-spoken, adaptive, optimistic, quietly authentic and emotionally scarred. "
     },
     csky = {
         name = "Clear Sky",
-        style = "a member of Clear Sky, a faction who want to understand the Zone better. They are idealistic, optimistic and cautious. "
+        style = "member of Clear Sky, a faction who want to understand the Zone better. They are idealistic, optimistic and cautious. "
     },
     ecolog = {
         name = "Ecolog",
-        style = "a member of the Ecolog faction. They are scientists performing field research in The Zone and are curious, open-minded, cautious and dislike violence. " 
+        style = "member of the Ecolog faction. They are scientists performing field research in The Zone and are curious, open-minded, cautious and dislike violence. " 
     },
     army = {
         name = "Army",
-        style = "a member of the Army faction. They are soldiers in the Ukranian army who are deployed to the Zone to keep the public out for their own safety, as well as to protect the government-funded scientists of the Ecolog faction. They are reluctant, undisciplined, bitter and emotionally burned-out. Use military terminology, vernacular and slang. "
+        style = "member of the Army faction. They are soldiers in the Ukranian army who are deployed to the Zone to keep the public out for their own safety, as well as to protect the government-funded scientists of the Ecolog faction. They are reluctant, undisciplined, bitter and emotionally burned-out. Use military terminology, vernacular and slang. "
     },
     renegade = {
         name = "Renegade",
-        style = "a member of the Renegade faction. They are violent criminals who have committed acts so despicable even the Bandit faction barely tolerates them. They are not just ruthless and violent but depraved, vile, crass, deplorable and erratic. Use explicit language. "
+        style = "member of the Renegade faction. They are violent criminals who have committed acts so despicable even the Bandit faction barely tolerates them. They are not just ruthless and violent but depraved, vile, crass, deplorable and erratic. Use explicit language. "
     },
     trader = {
         name = "Trader",
-        style = "a trader and merchant and is persuasive, a little greedy and sometimes resorts to flattery. "
+        style = "trader and merchant and is persuasive, a little greedy and sometimes resorts to flattery. "
     },
     greh = {
         name = "Sin",
-        style = "a member of the Sin faction, a religious sect of zombified stalkers worshipping the Zone. They believe the Zone is of divine nature and see it as humanity's path to ultimate redemption. They are creepy, possessed and ritualistic. "
+        style = "member of the Sin faction, a religious sect of zombified stalkers worshipping the Zone. They believe the Zone is of divine nature and see it as humanity's path to ultimate redemption. They are creepy, possessed and ritualistic. "
     },
     isg = {
         name = "ISG",
-        style = "a member the UNISG faction, an elite Spec Ops Recon unit under the United Nations gathering intel in the Zone. They are elitist, hostile, calculating and distrustful of other factions except Mercenaries. "
+        style = "member the UNISG faction, an elite Spec Ops Recon unit under the United Nations gathering intel in the Zone. They are elitist, hostile, calculating and distrustful of other factions except Mercenaries. "
     },
     zombied = {
         name = "Zombied",
