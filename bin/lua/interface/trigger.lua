f02b892506ba609245757a850dba0f38d64e570f
Coelacanthiform <coelacanthiform@proton.me>
2025-12-09 14:00:11 +0100

    Add files via upload

---
+++ bin/lua/interface/trigger.lua
@@ -0,0 +1,25 @@
+local log = require("framework.logger")
+
+local c = talker_game_commands
+
+local m = {}
+
+function m.talker_player_speaks(dialogue)
+    log.debug("Calling trigger talker_player_speaks with arg: %s", dialogue)
+    c.SendScriptCallback("talker_player_speaks", dialogue)
+end
+
+function m.talker_game_event(unformatted_description, event_objects, witnesses, important, flags)
+    c.SendScriptCallback("talker_game_event", unformatted_description, event_objects, witnesses, important, flags)
+end
+
+function m.talker_game_event_near_player(unformatted_description, involved_objects, important, flags)
+    c.SendScriptCallback("talker_game_event_near_player", unformatted_description, involved_objects, important, flags)
+end
+
+function m.talker_character_instructions(unformatted_description, character, important, flags)
+    c.SendScriptCallback("talker_game_event_near_player", unformatted_description, {character}, important, flags)
+    return true
+end
+
+return m
