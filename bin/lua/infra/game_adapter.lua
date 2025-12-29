d8f951cb115b286295843070ca7d57f1028bc888
Coelacanthiform <coelacanthiform@proton.me>
2025-12-29 20:37:50 +0100

    Update game_adapter.lua

--- bin/lua/infra/game_adapter.lua
+++ bin/lua/infra/game_adapter.lua
@@ -3,10 +3,10 @@ package.path = package.path .. ";./bin/lua/?.lua;"
 local Event = require("domain.model.event")
 local Character = require("domain.model.character")
 local Item = require("domain.model.item")
-local log = require('framework.logger')
-local config = require('interface.config')
+local log = require("framework.logger")
+local config = require("interface.config")
 
-require('infra.STALKER.factions')
+require("infra.STALKER.factions")
 -- game interfaces
 local query = talker_game_queries
 local game_async = talker_game_async
@@ -14,7 +14,7 @@ local command = talker_game_commands
 
 -- in the mod, IDs are always strings
 local function get_id(obj)
-    return tostring(query.get_id(obj))
+	return tostring(query.get_id(obj))
 end
 
 local m = {}
@@ -24,41 +24,46 @@ local m = {}
 ------------------------------------------------------------
 
 function m.get_characters_near(obj, distance)
-    local nearby_character_objs = query.get_nearby_characters(obj, distance)
-    return gameObj_to_characters(nearby_character_objs)
+	local nearby_character_objs = query.get_nearby_characters(obj, distance)
+	return gameObj_to_characters(nearby_character_objs)
 end
 
 function m.get_characters_near_player(distance)
-    local player_obj = query.get_player()
-    return m.get_characters_near(player_obj, distance)
+	local player_obj = query.get_player()
+	if not player_obj then
+		return {}
+	end
+	return m.get_characters_near(player_obj, distance)
 end
 
 function m.get_companions()
-    local companion_objs = query.get_companions()
-    return gameObj_to_characters(companion_objs)
+	local companion_objs = query.get_companions()
+	return gameObj_to_characters(companion_objs)
 end
 
 function m.get_player_character()
-    local player_obj = query.get_player()
-    return m.create_character(player_obj)
+	local player_obj = query.get_player()
+	return m.create_character(player_obj)
 end
 
 function m.get_name_by_id(game_id)
-    local game_obj = query.get_obj_by_id(game_id)
-    return query.get_name(game_obj)
+	local game_obj = query.get_obj_by_id(game_id)
+	return query.get_name(game_obj)
 end
 
 function m.get_character_by_id(game_id)
-    local game_obj = query.get_obj_by_id(game_id)
-    if game_obj then
-        return m.create_character(game_obj)
-    end
-    return nil
+	local game_obj = query.get_obj_by_id(game_id)
+	if game_obj then
+		return m.create_character(game_obj)
+	end
+	return nil
 end
 
 function m.get_name(game_obj)
-    if not game_obj then return "Unknown" end
-    return query.get_name(game_obj)
+	if not game_obj then
+		return "Unknown"
+	end
+	return query.get_name(game_obj)
 end
 
 ------------------------------------------------------------
@@ -66,59 +71,99 @@ end
 ------------------------------------------------------------
 
 function gameObj_to_characters(gameObjs)
-    local characters = {}
-    for _, character in ipairs(gameObjs) do
-        table.insert(characters, m.create_character(character))
-    end
-    return characters
+	local characters = {}
+	for _, character_obj in ipairs(gameObjs) do
+		local char = m.create_character(character_obj)
+		-- Only add if character creation succeeded (not nil)
+		if char then
+			table.insert(characters, char)
+		end
+	end
+	return characters
 end
 
-
 function m.create_game_event(unformatted_description, involved_objects, witnesses, flags)
-    local game_time = query.get_game_time_ms()
-    local world_context = query.describe_world()
-    local new_event = Event.create_event(unformatted_description, involved_objects, game_time, world_context, witnesses, flags)
-    return new_event
+	local game_time = query.get_game_time_ms()
+	local world_context = query.describe_world()
+	local new_event =
+		Event.create_event(unformatted_description, involved_objects, game_time, world_context, witnesses, flags)
+	return new_event
 end
 
 function m.create_character(game_object_person)
-    if not game_object_person then
-        log.warn('create_character called with nil object')
-        return nil
-    end
-    local game_id = get_id(game_object_person)
-    local name = query.get_name(game_object_person)
-    local experience = query.get_rank(game_object_person)
-    local faction = get_faction_name(query.get_faction(game_object_person)) or "unknown"
-    local weapon = query.get_weapon(game_object_person)
-    local weapon_description = nil
-    if weapon then
-        weapon_description = query.get_item_description(weapon)
-    end
-    log.spam('creating character with id: ' .. game_id .. ', name: ' .. name .. ', experience: ' .. experience .. ', faction: ' .. faction)
-    return Character.new(game_id, name, experience, faction, weapon_description)
+	if not game_object_person then
+		log.warn("create_character called with nil object")
+		return nil
+	end
+	local game_id = get_id(game_object_person)
+	local name = query.get_name(game_object_person)
+	local experience = query.get_rank(game_object_person)
+	local faction = get_faction_name(query.get_faction(game_object_person)) or "unknown"
+	local weapon = query.get_weapon(game_object_person)
+	local weapon_description = nil
+	if weapon then
+		weapon_description = query.get_item_description(weapon)
+	end
+	log.spam(
+		"creating character with id: "
+			.. game_id
+			.. ", name: "
+			.. name
+			.. ", experience: "
+			.. experience
+			.. ", faction: "
+			.. faction
+	)
+	return Character.new(game_id, name, experience, faction, weapon_description)
 end
 
 function m.get_player_weapon()
-    local player_obj = query.get_player()
-    local weapon_obj = query.get_weapon(player_obj)
-    local weapon = m.create_item(weapon_obj)
-    return weapon
+	local player_obj = query.get_player()
+	local weapon_obj = query.get_weapon(player_obj)
+	local weapon = m.create_item(weapon_obj)
+	return weapon
 end
 
 function m.create_item(game_object_item)
-    local game_id = get_id(game_object_item)
-    local name = query.get_item_description(game_object_item)
-    return Item.new(game_id, name)
+	local game_id = get_id(game_object_item)
+	local name = query.get_item_description(game_object_item)
+	return Item.new(game_id, name)
 end
 
-function m.create_dialogue_event(speaker_id, dialogue)
-    log.debug('creating dialogue event')
-    local speaker_obj = query.get_obj_by_id(speaker_id)
-    local witnesses = m.get_characters_near(speaker_obj)
-    local speaker_char = m.create_character(speaker_obj)
-    local dialogue_event = m.create_game_event("%s, a %s rank member of the %s faction said: %s", {speaker_char.name, speaker_char.experience, speaker_char.faction, dialogue}, witnesses)
-    return dialogue_event
+function m.create_dialogue_event(speaker_id, dialogue, source_event)
+	log.debug("creating dialogue event")
+	local speaker_obj = query.get_obj_by_id(speaker_id)
+	local speaker_char = m.create_character(speaker_obj)
+
+	-- If speaker character creation failed, abort
+	if not speaker_char then
+		log.warn("Failed to create speaker character for dialogue event")
+		return nil
+	end
+
+	-- Determine witnesses based on is_whisper flag
+	local witnesses
+	local flags = {}
+
+	-- Propagate is_whisper flag from source event
+	if source_event and source_event.flags and source_event.flags.is_whisper then
+		-- Whisper mode: only companions can witness
+		witnesses = m.get_companions()
+		flags.is_whisper = true
+		log.debug("Creating whisper dialogue event with companion-only witnesses")
+	else
+		-- Normal mode: nearby characters can witness
+		witnesses = m.get_characters_near(speaker_obj)
+		log.debug("Creating normal dialogue event with nearby witnesses")
+	end
+
+	local dialogue_event = m.create_game_event(
+		"%s, a %s rank member of the %s faction said: %s",
+		{ speaker_char.name, speaker_char.experience, speaker_char.faction, dialogue },
+		witnesses,
+		flags
+	)
+	return dialogue_event
 end
 
 ------------------------------------------------------------
@@ -126,65 +171,63 @@ end
 ------------------------------------------------------------
 -- ASYNCs
 function m.repeat_until_true(seconds, func, ...)
-    game_async.repeat_until_true(seconds, func, ...)
+	game_async.repeat_until_true(seconds, func, ...)
 end
 
 -- DIALOGUE
 function m.display_dialogue(speaker_id, dialogue)
-    log.debug('displaying dialogue')
-    command.display_message(speaker_id, dialogue)
+	log.debug("displaying dialogue")
+	command.display_message(speaker_id, dialogue)
 end
 
 function m.display_error_to_player(message)
-    command.display_hud_message(message, 3)
+	command.display_hud_message(message, 3)
 end
 
 function m.display_to_player(message, seconds)
-    if not config.SHOW_HUD_MESSAGES then
-        return
-    end
-    seconds = seconds or 7
-    command.display_hud_message(message, seconds)
+	if not config.SHOW_HUD_MESSAGES then
+		return
+	end
+	seconds = seconds or 7
+	command.display_hud_message(message, seconds)
 end
 
 function m.is_cooldown_over(LAST_GAME_TIME_MS, CD_MS)
-    return (query.get_game_time_ms() - LAST_GAME_TIME_MS > CD_MS)
+	return (query.get_game_time_ms() - LAST_GAME_TIME_MS > CD_MS)
 end
 
 function m.get_distance(obj_id1, obj_id2)
-    local obj1 = query.get_obj_by_id(obj_id1)
-    local obj2 = query.get_obj_by_id(obj_id2)
-    return query.get_distance_between(obj1, obj2)
+	local obj1 = query.get_obj_by_id(obj_id1)
+	local obj2 = query.get_obj_by_id(obj_id2)
+	return query.get_distance_between(obj1, obj2)
 end
 
 function m.get_distance_to_player(obj_id)
-    local obj = query.get_obj_by_id(obj_id)
-    local player = query.get_player()
-    return query.get_distance_between(obj, player)
+	local obj = query.get_obj_by_id(obj_id)
+	local player = query.get_player()
+	return query.get_distance_between(obj, player)
 end
 
 -- to be moved
 function m.is_player(character_id)
-    return tostring(character_id) == "0"
+	return tostring(character_id) == "0"
 end
 
-
-local game_files = talker_game_files or require('tests.mocks.mock_game_queries') -- todo improve
-
+local game_files = talker_game_files or require("tests.mocks.mock_game_queries") -- todo improve
 
 -- I hotfixed this due to a loop dependency but it may also be possible that this function m.get_base_path() was just broken and requiring all of game_query
 -- FILES
 function m.get_base_path()
-    return game_files.get_base_path()
+	return game_files.get_base_path()
 end
 
-local is_test_env, mock_game_adapter = pcall(require, 'tests.mocks.mock_game_adapter')
+local is_test_env, mock_game_adapter = pcall(require, "tests.mocks.mock_game_adapter")
 if false and is_test_env then
-    return mock_game_adapter
+	return mock_game_adapter
 end
 
 function m.is_mock()
-    return false
+	return false
 end
 
 return m
