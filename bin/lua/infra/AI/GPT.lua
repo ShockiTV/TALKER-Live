76f92807a68fef626819c8ae0fb987da5fad025e
Coelacanthiform <coelacanthiform@proton.me>
2026-01-09 21:22:52 +0100

    Update GPT.lua

--- bin/lua/infra/AI/GPT.lua
+++ bin/lua/infra/AI/GPT.lua
@@ -80,7 +80,7 @@ function gpt.pick_speaker(msgs, callback)
 end
 
 function gpt.summarize_story(msgs, callback)
-  return send(msgs, callback, {model=MODEL.fast, temperature=0.2, max_tokens=1500})
+  return send(msgs, callback, {model=MODEL.fast, temperature=0.2, max_tokens=2500})
 end
 
 return gpt
