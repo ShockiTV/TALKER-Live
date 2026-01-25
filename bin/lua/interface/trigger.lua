e476aa5d40cf1c7007d1b6599c5de743fea0a1c0
Coelacanthiform <coelacanthiform@proton.me>
2026-01-25 17:43:04 +0100

    Added silent events

--- bin/lua/interface/trigger.lua
+++ bin/lua/interface/trigger.lua
@@ -27,4 +27,12 @@ function m.talker_character_instructions(unformatted_description, character, imp
 	return true
 end
 
+function m.talker_silent_event_near_player(unformatted_description, involved_objects, flags)
+	c.SendScriptCallback("talker_silent_event_near_player", unformatted_description, involved_objects, flags)
+end
+
+function m.talker_silent_event(unformatted_description, event_objects, witnesses, flags)
+	c.SendScriptCallback("talker_silent_event", unformatted_description, event_objects, witnesses, flags)
+end
+
 return m
