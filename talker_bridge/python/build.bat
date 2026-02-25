4077d7904e3d13ef56d533e0739e7de68d299727
Robert Simon <shock.shock@gmail.com>
2026-02-26 00:34:49 +0100

    feat: update build script and add launch_talker_bridge.bat
    - Remove whisper/zmq hidden-imports from build.bat, rename output to talker_bridge
    - Create simplified launch_talker_bridge.bat (no provider menu needed)
    - Mark tasks 6.1-6.2 complete

--- talker_bridge/python/build.bat
+++ talker_bridge/python/build.bat
@@ -1,1 +1,1 @@
-python -m PyInstaller --onefile --name talker_mic --icon=talker_mic.ico --hidden-import=zmq --hidden-import=gemini_proxy --hidden-import=whisper_local --hidden-import=whisper_api main.py
+python -m PyInstaller --onefile --name talker_bridge --icon=talker_mic.ico --hidden-import=gemini_proxy main.py
